00301ef294d77ff7b853b5e7a3d36d98e91d1fe4
daniluus <55250414+daniluus@users.noreply.github.com>
2021-09-04 00:10:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Elements
+/* Todos que tem a class .btn passa para elements */
+const elements = document.querySelectorAll('.btn');
+
+
+// Event
+/**
+ * 
+ */
+elements.forEach(element => {
+    element.addEventListener('click', () => {
+        let command = element.dataset['element'];
+
+        if(command == 'createLink' || command == 'insertImage'){
+            let url = prompt('Enter the link here: ', 'http://');
+            document.execCommand(command, false, url);
+        } else {
+            document.execCommand(command, false, null)
+        }
+
+        
+    })
+})
